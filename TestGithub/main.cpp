c63e217ea5ea7857681d7541e0db18940bd044e7
Hadrien62 <hadrien.delobel@student.junia.com>
2023-03-18 13:51:20 +0100

    Initial Commit

---
+++ TestGithub/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TestGithub
+//
+//  Created by Hadrien Delobel on 18/03/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
